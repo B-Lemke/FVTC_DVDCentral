8a9c3914281dd9c6cae4032d437132e5220ceedc
Brody Lemke <500161066@fvtc.edu>
2019-02-20 14:02:49 -0600

    Updating tblOrder Fields

--- BJL.DVDCentral/BJL.DVDCentral.DB/Tables/tblOrder.sql
+++ BJL.DVDCentral/BJL.DVDCentral.DB/Tables/tblOrder.sql
@@ -3,6 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [CustomerId] INT NOT NULL, 
     [OrderDate] DATETIME NOT NULL, 
-    [PaymentReceipt] DATETIME NOT NULL, 
-    [ShipDate] DATETIME NOT NULL
+    [PaymentReceipt] VARCHAR(50) NOT NULL, 
+    [ShipDate] DATETIME NOT NULL, 
+    [UserId] INT NOT NULL
 )
